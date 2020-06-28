1faa3c980ef02dc8b44c6c615c57c3384ff7fa87
Pranav Patel <pranav.ec@gmail.com>
2020-06-28 05:55:20 -0400

    multiple-reducers app commit

---
+++ multiple-reducers/src/index.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+
+import { createStore, combineReducers } from 'redux';
+import { Provider } from 'react-redux';
+
+//import reducer from './store/reducer';
+import reducerA from './store/reducerA';
+import reducerB from './store/reducerB';
+
+const rootReducer = combineReducers({
+  rA: reducerA,
+  rB: reducerB,
+});
+
+const store = createStore(rootReducer);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
